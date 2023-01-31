fa8e0859c23869cc537f58c0956ac1862a6a0ddb
HemanshuYPatil <110720608+HemanshuYPatil@users.noreply.github.com>
2023-01-31 09:36:13 +0530

    Add files via upload

---
+++ Iope_Login/Experiment/ExperimentNo.1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Experiment No.1</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../experiment.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
+<body>
+    <section id="home">
+  <nav class="navbar">
+    <div class="content">
+      <div class="logo">
+        <a href="#">I.O.P.E</a>
+      </div>
+      <ul class="menu-list">
+        <li><a href="../iope.php ">Home</a></li>
+        <li><a href="../iope.php ">About</a></li>
+        <li ><a href="../iope.php ">Experiment</a></li>
+      </ul>
+    </div>
+    
+</nav>
+
+</section>
+
+<section id="experiment1">
+    <div class="experiment1">
+        <h2>Experiment No.1</h2><br><br>
+    <div class="btn">
+        <a href="Exp1_aim.php">Aim</a>
+        <!-- <a href="">Theory</a> -->
+        <a href="Exp1_code.php">Code</a>
+        <a href="Exp1_con.php">Conclusion</a>
+        <a href="../Examples/Exp1_examle.php">Examples</a>
+    </div>
+    </div>
+</section>
+
+
+<section class="footer" style="margin-top: 32.5%;">
+  
+
+  <ul class="list">
+    <li>
+      <a href="../iope.php ">Home</a>
+    </li>
+    <li>
+      <a href="../iope.php ">About</a>
+    </li>
+    <li>
+      <a href="../iope.php ">Experiment</a>
+    </li>
+
+  </ul>
+  <p class="designer">Designed by Hemanshu Patil</p>
+</section>
+
+</body>
+</html>
