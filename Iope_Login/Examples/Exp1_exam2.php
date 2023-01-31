72507e7395ce45afbb449031936f6c6d5b936994
HemanshuYPatil <110720608+HemanshuYPatil@users.noreply.github.com>
2023-01-31 09:37:51 +0530

    Add files via upload

---
+++ Iope_Login/Examples/Exp1_exam2.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examples</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../experiment.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
+<body>
+    <section id="home">
+  <nav class="navbar">
+    <div class="content">
+      <div class="logo">
+        <a href="#">I.O.P.E</a>
+      </div>
+      <ul class="menu-list">
+        <li><a href="../iope.php ">Home</a></li>
+        <li><a href="../iope.php ">About</a></li>
+        <li ><a href="../iope.php ">Experiment</a></li>
+      </ul>
+    </div>
+    
+</nav>
+
+</section>
+
+<section id="experiment1">
+    <div class="experiment1">
+        <h2>Experiment No.1</h2><br><br>
+    <div class="btn">
+        <a href="Exp1_ex.php">Example 1</a>
+        <a href="Exp1_exam2.php">Example 2</a>
+        <a href="Exp1_exam3.php">Example 3</a>
+        <a href="Exp1_exam4 .php">Example 4</a>
+    </div>
+    </div>
+</section>
+
+<section>
+    <div class="code">
+       <h2>Code :-</h2><br>
+       <xmp>
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <title> My First Webpage</title>
+                <body>
+                <font color = RED> <center>
+                <b>*** Block level tags*** </b> </center> </font>
+                <font color = RED>
+                <p> <center> Samyak Fulzele , Badlapur , My last year pointer is 9.0 </p> </center> </font>
+                <center> <H1> INSTITUTE OF PETROCHEMICAL ENGINEERING,LONERE </H1> </center>
+                <center> <H2> INFORMATION TECHNOLOGY </H2> </center>
+                <center> <H3>Samyak Fulzele</H3> </center>
+                <center> <H4> Badlapur </H4> </center>
+                <center> <H5> These are reading tags </H5> </center>
+                <center> <H6> These are heading tags </H6> </center>
+                <Hr color = blue>
+                <center> <H3> ***TEXT LEVEL TAGS*** </H3> </center>
+                <center> <b> Use of bold style </b> </br> </center>
+                <center> <i> Use of italic style </i> </br> </ center>
+                <center> <U>Use of underline tag </U> </br>  </center>
+                <center> <strike> This is strike text style </strike> </br> </center>
+                <center> <small> This is small text style </small> </br> </center>
+                <center> <big>This is big text style </ big> </br> </center>
+                <center> My name is <sub>Samyak Fulzele </sub> </br> </center>
+                <center> these <del> iss </del> <add> are </add> delete and add style tags </center>
+                </body>
+                </head>
+                </html>
+    </xmp>
+    <p style="margin-left: 77rem; font-size: 17px;">--Samyak Fulzele</p>
+
+       <h2 class="h">Output :-</h2>
+       <!-- <img style="border: 2px solid black;" src="../assets/expcode1.png"> -->
+       <p class="paragraph">Click on the below button to see the output.</p>
+       <div class="experiment1">
+    <div class="btn1">
+        <a href="../assets/exp1example2.png" target="_blank">Click Here</a>
+    </div>
+    </div>
+</section>
+
+<section class="footer" style="margin-top: 6.5%;">
+  
+
+  <ul class="list">
+    <li>
+      <a href="../iope">Home</a>
+    </li>
+    <li>
+      <a href="../iope.php ">About</a>
+    </li>
+    <li>
+      <a href="../iope.php ">Experiment</a>
+    </li>
+
+  </ul>
+  <p class="designer">Designed by Hemanshu Patil</p>
+</section>
+
+</body>
+</html>
---
+++ Iope_Login/Examples/Exp1_examle.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Examples</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../experiment.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+</head>
+<body>
+    <section id="home">
+  <nav class="navbar">
+    <div class="content">
+      <div class="logo">
+        <a href="#">I.O.P.E</a>
+      </div>
+      <ul class="menu-list">
+        <li><a href="../iope.php ">Home</a></li>
+        <li><a href="../iope.php ">About</a></li>
+        <li ><a href="../iope.php ">Experiment</a></li>
+      </ul>
+    </div>
+    
+</nav>
+
+</section>
+
+<section id="experiment1">
+    <div class="experiment1">
+        <h2>Experiment No.1</h2><br><br>
+    <div class="btn">
+        <a href="Exp1_ex.php">Example 1</a>
+        <a href="Exp1_exam2.php">Example 2</a>
+        <a href="Exp1_exam3.php">Example 3</a>
+        <a href="Exp1_exam4 .php">Example 4</a>
+    </div>
+    </div>
+</section>
+
+
+<section class="footer" style="margin-top: 32.5%;">
+  
+
+  <ul class="list">
+    <li>
+      <a href="../iope.php ">Home</a>
+    </li>
+    <li>
+      <a href="../iope.php ">About</a>
+    </li>
+    <li>
+      <a href="../iope.php ">Experiment</a>
+    </li>
+
+  </ul>
+  <p class="designer">Designed by Hemanshu Patil</p>
+</section>
+
+</body>
+</html>
